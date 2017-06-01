030c24b3bc417ce8886a33a8137010b4ea86a453
majorinche <594216467@qq.com>
2017-06-01 08:56:33 +0800

    add some comment

--- cat.c
+++ cat.c
@@ -7,7 +7,7 @@ static char             *infile = NULL;
 static int              input_desc = -1;       
 static void _cat(int fileno)
 {
-    char                buf[MAX_BUF_SIZE];
+    char                buf[MAX_BUF_SIZE];/*why does buf need value?*/
     int                 n;
     n = read(fileno, buf, MAX_BUF_SIZE);
     write(STDOUT_FILENO, buf, n);
@@ -16,10 +16,10 @@ static void _cat(int fileno)
 static void simple_cat(char *file)
 {
     input_desc = open(file, O_RDONLY);
-    _cat(input_desc);
+    _cat(input_desc); /*why need underscore stuff*/
     close(input_desc);
 }
-int main(int argc,char *argv[])
+int main(int argc,char *argv[])/*argv should exist with argv[] together*/
 {
     infile = argv[1];
     simple_cat(infile);
