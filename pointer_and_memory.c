6d4dca3472b61d52d5aec4dbe15dbb1b6a9995f8
majorinche <594216467@qq.com>
2017-10-26 21:05:01 +0800

    add commit about the result

--- pointer_and_memory.c
+++ pointer_and_memory.c
@@ -5,6 +5,7 @@ void main(){
 	int *pa=&a; //pa means pointer of variable a
 	int *pb=&b; //pb means pointer of variable b
 	printf("a ma is: %08x and  b ma is: %08x",pa,pb);//i'd like to print out the memory address, seems not easy
+	// the result is a ma is: 03748b9c and  b ma is: 03748b98, different as gdb side, but the values seems right
 }
 /*
 (gdb) start
